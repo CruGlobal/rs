d4cc47d4f94109cd42e5ec4a1f5cee245be3dca3
Josh Starcher <josh.starcher@gmail.com>
2013-12-21 08:08:55 -0500

    Send emails from cru.org

--- app/mailers/email.rb
+++ app/mailers/email.rb
@@ -1,6 +1,6 @@
 class Email < ActionMailer::Base
-	default :sender => 'no-reply@ride.uscm.org', :content_type => "text/html"
-	
+	default :sender => 'no-reply@cru.org', :content_type => "text/html"
+
 	def car(driver_ride_id)
 		@driver = Ride.find(driver_ride_id)
 		emails = []
@@ -8,9 +8,9 @@ class Email < ActionMailer::Base
 		@driver.rides.each do |rider|
 			emails << rider.email
 		end
-		
+
 		@event = Event.find(@driver.event_id)
-		
+
 		mail(:to => emails.join(','), :subject => 'Ride information for ' + @event.event_name )
 	end
 
